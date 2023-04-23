0a315d96b3820cf273474e65217b1bd309d504ed
Dada Clement Authority <dadaauthourity23@gmail.com>
2023-04-23 23:34:55 +0100

    added shell.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - simple shell main function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	shell();
+	return (0);
+}
---
+++ shell.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+//#include <sys/wait.h>
+/**
+ * shell - the main shell execute program file
+ *
+ * Return: Always.
+ */
+int shell()
+{
+	
+
+	printf("$ ");
+}
